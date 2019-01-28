a5375c89f7ba21b05961cb5bc794acc06101ec7e
Chih-Lin Lee <mlee749@gmail.com>
2019-01-28 12:17:53 -0800

    finished setting up server and react environment

---
+++ 01 - JavaScript Drum Kit/client/src/components/DrumKit.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class DrumKit extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>Testing </div>
+    )
+  }
+}
+
+export default DrumKit;
---
+++ 01 - JavaScript Drum Kit/client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DrumKit from './components/DrumKit.jsx';
+
+ReactDOM.render(<DrumKit />, document.getElementById('drum'));
